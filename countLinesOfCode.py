affc07594496207064f5e3916163bd377018e374
Kevin H <kevin.harbarth@gmx.net>
2017-01-17 16:13:44 +0100

    Added support for subdirectories and Error handling if a file cant be opened

--- countLinesOfCode.py
+++ countLinesOfCode.py
@@ -4,12 +4,30 @@ import os
 
 def countLines(files):
     for file in files:
+        try:
+            f = open(file)
+            yield sum(1 for _ in f)
+        except Exception as e:
+            print("Error opening file: " + str(file))
+            # print(e)
+        else:
+            f.close
+
+"""No error handling!"""
+def countLinesShort(files):
+    for file in files:
         with open(file) as f:
             yield sum(1 for _ in f)
 
 
-root = './'
-for path, subdirs, files in os.walk(root):
-    print(sum(countLines(files)))
+def listFiles(root):
+    for dirpath, dirnames, filenames in os.walk(root):
+        for name in [os.path.join(dirpath, file) for file in filenames]:
+            yield name
 
+
+root = './'
+fileList = list(listFiles(root))
+lines = list(countLines(fileList))
+print(sum(lines))
 input("Press any key to continue...")
