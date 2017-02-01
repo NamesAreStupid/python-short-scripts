3ea39ffe622b00841e956d2c0cb42e88fe5b2999
Kevin H <kevin.harbarth@gmx.net>
2017-02-01 22:13:43 +0100

    Some perceptron shit

--- MachineLearning/perceptron.py
+++ MachineLearning/perceptron.py
@@ -58,7 +58,4 @@ class Perceptron(object):
     def predict(self, X):
         """Return class label after unit step"""
         return np.where(self.net_input(X) >= 0.0, 1, -1)
-
-
-df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
-df.tail()
+        
